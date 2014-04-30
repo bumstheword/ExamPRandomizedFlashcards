13bda2120302ce9441af300e408b35ef1847a2f4
bumstheword <mrg58@georgetown.edu>
2014-04-30 16:37:30 -0400

    choose section

--- FlaschardsRandomizer.py
+++ FlaschardsRandomizer.py
@@ -9,16 +9,18 @@ goulet.michaelrobert@gmail.com
 
 from PyPDF2 import PdfFileWriter, PdfFileReader
 import sys
+import random
 
 sys.setrecursionlimit(2000)
 STARTPAGE = [3,37,47,49,63,91,119,131,151,153,157,191,213,225,247,267,289,311,321,327,349,367,399,417]
 ENDPAGE = [36,46,48,60,90,118,130,150,152,156,190,212,222,246,266,288,310,320,326,348,364,398,416,422]
-SECTION11 = list(range(3,37))
-SECTION12 = list(range(37,47))
-SECTION13 = list(range(47,49))
-SECTION14 = list(range(49, 61))
-SECTION21
-SECTION22
 
-inputpdf = PdfFileReader(open("C:/Users/Rochester/DesktopExamPFlashcards.pdf", "rb"))
+inputpdf = PdfFileReader(open("C:/Users/Rochester/Desktop/ExamPFlashcards.pdf", "rb"))
 
+#choose a random number between 0 and length of STARTPAGE, which corresponds to section
+section = random.sample(range(len(STARTPAGE)),1)[0]
+#calculate length of section
+section_length = (ENDPAGE[section] - STARTPAGE[section] + 1)
+
+print(section)
+print(STARTPAGE[section], ENDPAGE[section], section_length)
