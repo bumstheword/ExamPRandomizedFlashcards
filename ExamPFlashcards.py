c013b2b5c1bd7b2a2e2ffa0ff80a4143d03b8b44
bumstheword <mrg58@georgetown.edu>
2014-04-30 15:06:23 -0400

    python split code

---
+++ ExamPFlashcards.py
@@ -0,0 +1,24 @@
+"""
+Coaching Actuaries
+Exam P Mobile Flashcards Randomization Program
+
+Michael Goulet
+Profile Insurance Group
+goulet.michaelrobert@gmail.com
+"""
+
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import sys
+
+sys.setrecursionlimit(2000)
+
+inputpdf = PdfFileReader(open("C:/Users/Rochester/Desktop/ExamPFlashcards.pdf", "rb"))
+
+for i in range(0,inputpdf.numPages,2):
+    output = PdfFileWriter()
+    output.addPage(inputpdf.getPage(i))
+    output.addPage(inputpdf.getPage(i + 1))
+    m = i / 2
+    outputStream = open("C:/Users/Rochester/Documents/ExamP/page%s.pdf" % m, "wb")
+    output.write(outputStream)
+    outputStream.close()
